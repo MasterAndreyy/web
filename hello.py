c5e3a79acc060d79df44c318e98c893755a47377
Zakharov Andrey Mikhaylovich <AMZakharov@gksm.local>
2019-11-03 13:10:56 +0300

    new

--- hello.py
+++ hello.py
@@ -1,9 +1,3 @@
-def wsgi_application(environ, start_response):
-status = '200 OK'
-headers = [
-('Content-Type', 'text/plain')
-]
-body = 'Hello, Andrey!'
-start_response(status, headers )
-return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),encoding="utf8")]
