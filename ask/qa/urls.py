bb8b55fa3fdd0e84a2d9334c92959bbe042f0482
Zakharov Andrey Mikhaylovich <AMZakharov@gksm.local>
2019-11-17 15:52:06 +0300

    new

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 from qa.views import test
 
-urlpatterns = [                                              
-   url(r'^$', 'test'),                                                              
-   url(r'^login/.*$', 'test', name='login'),                                    
-   url(r'^signup/.*', 'test', name='signup'),                                   
-   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
-   url(r'^ask/.*', 'test', name='ask'),                                         
-   url(r'^popular/.*', 'test', name='popular'),                                 
-   url(r'^new/.*', 'test', name='new'),                                         
+urlpatterns = [
+    url(r'^$', test, name='question_list'),
+    url(r'^question/(?P<pk>\d+)/', test, name='question_detail'),
+    url(r'^popular/', test, name='popular'),
+    url(r'^ask/', test, name='question_ask'),
+    url(r'^answer/', test, name='question_answer'),
+    url(r'^signup/', test, name='signup'),
+    url(r'^login/', test, name='login'),
+    url(r'^logout/', test, name='logout'),
+    url(r'^new/', test, name='new'),
 ]
