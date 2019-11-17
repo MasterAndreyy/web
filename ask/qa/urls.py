16f253e001c2629ea214e3777f0f2900a48e6852
Zakharov Andrey Mikhaylovich <AMZakharov@gksm.local>
2019-11-17 13:50:23 +0300

    urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,13 +4,13 @@ from qa.views import question_ask, question_answer
 from qa.views import user_signup, user_login, user_logout
 
 urlpatterns = [
-    url(r'^$', question_list, name='question_list'),
-    url(r'^question/(?P<pk>\d+)/', question_detail, name='question_detail'),
-    url(r'^popular/', popular, name='popular'),
-    url(r'^ask/', question_ask, name='question_ask'),
-    url(r'^answer/', question_answer, name='question_answer'),
-    url(r'^signup/', user_signup, name='signup'),
-    url(r'^login/', user_login, name='login'),
-    url(r'^logout/', user_logout, name='logout'),
+    url(r'^$', test, name='question_list'),
+    url(r'^question/(?P<pk>\d+)/', test, name='question_detail'),
+    url(r'^popular/', test, name='popular'),
+    url(r'^ask/', test, name='question_ask'),
+    url(r'^answer/', test, name='question_answer'),
+    url(r'^signup/', test, name='signup'),
+    url(r'^login/', test, name='login'),
+    url(r'^logout/', test, name='logout'),
     url(r'^new/', test, name='new'),
 ]
