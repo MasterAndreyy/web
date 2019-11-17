aef97d34b9edee264e91fd5b109de2991a8ecaec
Zakharov Andrey Mikhaylovich <AMZakharov@gksm.local>
2019-11-17 14:01:50 +0300

    new

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,14 +3,12 @@ from qa.views import test, question_list, question_detail, popular
 from qa.views import question_ask, question_answer
 from qa.views import user_signup, user_login, user_logout
 
-urlpatterns = [
-    url(r'^$', test, name='question_list'),
-    url(r'^question/(?P<pk>\d+)/', test, name='question_detail'),
-    url(r'^popular/', test, name='popular'),
-    url(r'^ask/', test, name='question_ask'),
-    url(r'^answer/', test, name='question_answer'),
-    url(r'^signup/', test, name='signup'),
-    url(r'^login/', test, name='login'),
-    url(r'^logout/', test, name='logout'),
-    url(r'^new/', test, name='new'),
-]
+urlpatterns = patterns('qa.views',                                              
+   url(r'^$', 'test'),                                                              
+   url(r'^login/.*$', 'test', name='login'),                                    
+   url(r'^signup/.*', 'test', name='signup'),                                   
+   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
+   url(r'^ask/.*', 'test', name='ask'),                                         
+   url(r'^popular/.*', 'test', name='popular'),                                 
+   url(r'^new/.*', 'test', name='new'),                                         
+)   
